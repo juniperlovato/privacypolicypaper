ee98102fc3dde24f3af4901c865a53a60b1ff0a3
Philip Mueller <mail@philip.in-aachen.net>
2023-02-04 21:49:45 -0500

    add regression lines to co-occurrence network distribution plots

--- analyze_coocurrence_graph.py
+++ analyze_coocurrence_graph.py
@@ -5,6 +5,8 @@ import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as stats
+import numpy as np
 
 args = argparse.ArgumentParser()
 args.add_argument('input', help='input graphml file')
@@ -21,11 +23,20 @@ degrees_df['rank'] = degrees_df['degree'].rank(ascending=False)
 
 input_basename = args.input.split('/')[-1].split('.')[0]
 
-plt.figure(constrained_layout=True)
+fig = plt.figure(constrained_layout=True)
 plt.title('Degree distribution')
 plt.ylabel('Degree')
 plt.xlabel('Rank')
 plt.plot(degrees_df['rank'], degrees_df['degree'], 'o', color='#2D9BF0')
+
+linear_regime = degrees_df[degrees_df['rank'] < 175]
+linear_regime = linear_regime.sort_values(by='rank')
+max_rank = degrees_df['rank'].max()
+
+# add regression line to linear plot, extend to the right
+slope, intercept, r_value, p_value, std_err = stats.linregress(linear_regime['rank'], linear_regime['degree'])
+plt.plot([0, max_rank], [intercept, slope*max_rank + intercept], color='#EA5F94')
+
 plt.savefig(input_basename + '.degree_distribution.pdf')
 plt.draw()
 
@@ -39,6 +50,18 @@ plt.title('Strength distribution')
 plt.ylabel('Strength')
 plt.xlabel('Rank')
 plt.semilogy(strengths_df['rank'], strengths_df['strength'], 'o', color='#2D9BF0')
+
+low = 25
+high = 200
+linear_regime = strengths_df[(strengths_df['rank'] >= low) & (strengths_df['rank'] <= high)]
+linear_regime = linear_regime.sort_values(by='rank')
+max_rank = degrees_df['rank'].max()
+
+# add regression line to linear plot, extend to the right
+# Careful: this is a plot with logarithmic y-axis and linear x-axis
+slope, intercept, r_value, p_value, std_err = stats.linregress(linear_regime['rank'], np.log10(linear_regime['strength']))
+plt.plot([0, max_rank], [10**intercept, 10**(intercept+slope*max_rank)], color='#EA5F94')
+
 plt.savefig(input_basename + '.strength_distribution.pdf')
 plt.draw()
 
